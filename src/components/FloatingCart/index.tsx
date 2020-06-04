f752e0c7410f8e286bed53da866a46f8592e8759
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-06-04 18:32:08 -0300

    [Bootcamp GoStack] Finalizando desafio: - Listar os produtos da fake API - Adicionar itens ao carrinho - Exibir itens do carrinho - Aumentar quantidade de itens do carrinho - Diminuir quantidade de um item do carrinho - Exibir valor total dos itens no carrinho

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,33 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const { total } = products.reduce(
+      (accumulator, { price, quantity }) => {
+        const individualSubTotal = price * quantity;
 
-    return formatValue(0);
+        accumulator.total += individualSubTotal;
+
+        return accumulator;
+      },
+      { total: 0 },
+    );
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const { totalQuantity } = products.reduce(
+      (accumulator, { quantity }) => {
+        accumulator.totalQuantity += quantity;
 
-    return 0;
+        return accumulator;
+      },
+      {
+        totalQuantity: 0,
+      },
+    );
+
+    return totalQuantity;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,41 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const { total } = products.reduce(
+      (accumulator, { price, quantity }) => {
+        const individualSubTotal = price * quantity;
 
-    return formatValue(0);
+        accumulator.total += individualSubTotal;
+
+        return accumulator;
+      },
+      { total: 0 },
+    );
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const { totalQuantity } = products.reduce(
+      (accumulator, { quantity }) => {
+        accumulator.totalQuantity += quantity;
 
-    return 0;
+        return accumulator;
+      },
+      {
+        totalQuantity: 0,
+      },
+    );
+
+    return totalQuantity;
   }, [products]);
 
   return (
